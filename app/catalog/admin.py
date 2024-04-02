154b69db8167b2460df3b2877c5a5e7e28cad507
nicahgaturi <nicahgaturi@gmail.com>
2024-04-02 23:06:47 +0300

    added import export functionality to admin.py

--- app/catalog/admin.py
+++ app/catalog/admin.py
@@ -1,26 +1,21 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin  # Import the  class
 from .models import Author, Genre, Book, BookInstance, Language
 
-# admin.site.register(Book)
-# admin.site.register(Author)
 admin.site.register(Genre)
-# admin.site.register(BookInstance)
 admin.site.register(Language)
 
 
 class BooksInline(admin.TabularInline):
-    """Defines format of inline book insertion (used in AuthorAdmin)"""
     model = Book
     extra = 0
 
 
-# Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
     inlines = [BooksInline]
 
-# Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
 
@@ -28,19 +23,17 @@ class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
     extra = 0
 
-# Register the Admin classes for Book using the decorator
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
 
+@admin.register(Book)
+class BookAdmin(ImportExportModelAdmin):  # Use ImportExportModelAdmin instead of ModelAdmin
+    list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
 
-# Register the Admin classes for BookInstance using the decorator
+
 @admin.register(BookInstance)
-class BookInstanceAdmin(admin.ModelAdmin):
+class BookInstanceAdmin(ImportExportModelAdmin):  # Use ImportExportModelAdmin instead of ModelAdmin
     list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
-
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
@@ -49,3 +42,4 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('status', 'due_back', 'borrower')
         }),
     )
+
