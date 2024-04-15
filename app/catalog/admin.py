31b7db7ba369f92b05bbda2288a090ad492f82c4
nicahgaturi <nicahgaturi@gmail.com>
2024-04-15 11:46:30 +0300

    diplay fields on list-display attribute with their respective data latest update

--- app/catalog/admin.py
+++ app/catalog/admin.py
@@ -1,24 +1,29 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ImportExportModelAdmin   # Import the  class
 from .models import Author, Genre, Book, BookInstance, Language
 
+# admin.site.register(Book)
+# admin.site.register(Author)
 
 
 admin.site.register(Genre)
+# admin.site.register(BookInstance)
 admin.site.register(Language)
 
 
 class BooksInline(admin.TabularInline):
+    #Defines format of inline book insertion (used in AuthorAdmin)"""
+
     model = Book
     extra = 0
 
-
+# Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
     inlines = [BooksInline]
 
-
+# Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
 
@@ -26,9 +31,9 @@ class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
     extra = 0
 
-
+# Register the Admin classes for Book using the decorator
 @admin.register(Book)
-class BookAdmin(ImportExportModelAdmin):
+class BookAdmin(ImportExportModelAdmin):# Use ImportExportModelAdmin instead of ModelAdmin
     list_display = ('title', 'author', 'display_genre')
 
     inlines = [BooksInstanceInline]
@@ -38,10 +43,10 @@ class BookAdmin(ImportExportModelAdmin):
 
     display_genre.short_description = 'Genre'
 
-
+# Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
-class BookInstanceAdmin(ImportExportModelAdmin):
-    list_display = ('book_title', 'status', 'borrower', 'due_back', 'id')
+class BookInstanceAdmin(ImportExportModelAdmin):# Use ImportExportModelAdmin instead of ModelAdmin
+    list_display = ('book_title', 'status', 'borrower', 'due_back', 'id')#use book_title instead of book
     list_filter = ('status', 'due_back')
 
     fieldsets = (
